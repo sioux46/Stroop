2a92551c9ea8d989d0642b3c6451ae010b0e684c
Sebastien Poitrenaud <poitrenaud@mac.com>
2021-03-20 16:35:14 +0100

    v0.39 url without .php

---
+++ bien/index.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html>
+  <head><title>STROOP bienveil SP-LutinUserlab</title></head>
+<body><script>location.href = '../stroopcore/index.php?condition=bienveil';</script></body>
+</html>
---
+++ susp/index.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html>
+  <head><title>STROOP malveil SP-LutinUserlab</title></head>
+<body><script>location.href = '../stroopcore/index.php?condition=malveil';</script></body>
+</html>
